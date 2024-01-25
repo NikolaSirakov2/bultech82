a4fc91b3f62422d7eb6ca103ce14d92cc9dc969e
NikolaSirakov2 <nikolasirakov1@gmail.com>
2024-01-25 21:17:24 +0200

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,35 +1,48 @@
-"use client"
+"use client";
 
-import { useState } from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import { NAV_LINKS } from '../constants';
+import styles from "../styles/Navbar.module.css";
+import { useState } from "react";
+import { useEffect, useRef } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { NAV_LINKS } from "../constants";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const menuRef = useRef(null);
 
   const toggleMenu = () => {
-    console.error("toggleMenu")
+    console.error("toggleMenu");
     setIsMenuOpen(!isMenuOpen);
   };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
     <nav className="flexBetween max-container padding-container relative z-30 py-5">
       <Link href="/">
         <Image src="/BultechLogo.jpg" alt="logo" width={104} height={49} />
       </Link>
-      <ul className={`hidden h-full gap-12 lg:flex ${isMenuOpen ? 'block' : ''}`}>
+
+      <ul className="hidden h-full gap-12 lg:flex">
         {NAV_LINKS.map((link) => (
-          <Link
-            href={link.href}
-            key={link.key}
-            className="regular-16 text-gray-50 cursor-pointer pb-1.5 transition-all hover:font-bold"
-          >
+          <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
             {link.label}
           </Link>
         ))}
       </ul>
-
+      
       <Image
         src="/menu.svg"
         alt="menu"
@@ -38,6 +51,24 @@ const Navbar = () => {
         className="inline-block cursor-pointer lg:hidden"
         onClick={toggleMenu}
       />
+      <div
+        ref={menuRef}
+        className={`${styles["menu-container"]} ${
+          isMenuOpen ? styles.open : ""
+        }`}
+      >
+        <ul className={styles.menu}>
+          {NAV_LINKS.map((link) => (
+            <Link
+              href={link.href}
+              key={link.key}
+              className="regular-16 text-gray-50 cursor-pointer pb-1.5 transition-all hover:font-bold"
+            >
+              {link.label}
+            </Link>
+          ))}
+        </ul>
+      </div>
     </nav>
   );
 };
