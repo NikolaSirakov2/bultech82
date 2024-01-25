fff28b801020ac10c086c01d084a447663bf30cc
NikolaSirakov2 <nikolasirakov1@gmail.com>
2024-01-25 21:17:20 +0200

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,29 +1,26 @@
 import React from "react";
+import style from "../styles/Hero.module.css";
+import Image from "next/image";
 
 function Hero() {
   return (
     <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flex-row">
-      {/* Left */}
-
       <div className="flex flex-col gap-10 md:gap-12 xl:gap-16">
-        {/* Title */}
-
+        
         <div className="flex flex-col gap-3">
           <h1 className="text-4xl font-bold text-gray-800 md:text-5xl xl:text-6xl">
             Bultech82 Ltd
           </h1>
 
-          <h3 className="text-2xl text-gray-600 md:text-3xl xl:text-4xl">
+          <h3
+            className={`${style.h3SmallScreen} text-2xl text-gray-600 md:text-3xl xl:text-4xl`}
+          >
             Bulgarian company for manufacturing technologies
           </h3>
         </div>
 
-        {/* Buttons */}
-
-        <div className="flex flex-col gap-4 md:flex-row">
-          <button className="btn btn-primary">Get Started</button>
-
-          <button className="btn btn-secondary">Learn More</button>
+        <div className={style.imageContainer}>
+          <Image src="/machineParts.jpg" alt="Machine Parts" layout="fill" objectFit="cover" />
         </div>
       </div>
     </section>
