2315ccf7ffa96861d6236ca610ec60cba0d30634
NikolaSirakov2 <nikolasirakov1@gmail.com>
2024-01-27 11:57:42 +0200

    Remove Pricing component, update Footer, and modify NAV_LINKS

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,6 @@ export default function Home() {
       <Hero/>
       <AboutUs/>
       <Services/>
-      <Pricing/>
       <ContactUs/>
     </>
   );
--- constants/index.ts
+++ constants/index.ts
@@ -3,7 +3,6 @@ export const NAV_LINKS = [
     { href: '/', key: 'home', label: 'Home' },
     { href: '#about-us', key: 'about_us', label: 'About Us' },
     { href: '#services', key: 'services', label: 'Services' },
-    { href: '#pricing', key: 'pricing ', label: 'Pricing ' },
     { href: '#contacts', key: 'contacts', label: 'Contacts' },
   ];
   
