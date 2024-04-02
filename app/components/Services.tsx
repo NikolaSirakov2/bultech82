79f6da629d35b723c4852ac80c6fc9c1bdf56f03
NikolaSirakov2 <nikolasirakov1@gmail.com>
2024-04-02 15:08:14 +0300

    Update Services.tsx

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -18,11 +18,6 @@ function Services() {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         
       </div>
-
-      <h2 className="text-4xl font-bold mt-8 mb-4">Post Processing</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-        
-      </div>
     </section>
   );
 }
