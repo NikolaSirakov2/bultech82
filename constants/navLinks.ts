daa382af14d810b043aad4d2bd284012e48b0250
NikolaSirakov2 <nikolasirakov1@gmail.com>
2024-04-02 15:08:19 +0300

    Update navLinks.ts

--- constants/navLinks.ts
+++ constants/navLinks.ts
@@ -1,6 +1,8 @@
 export const NAV_LINKS = [
-    { href: 'hero', label: 'Hero', key: '1' },
-    { href: 'about-us', label: 'About Us', key: '2' },
-    { href: 'services', label: 'Services', key: '3' },
-    { href: 'contact-us', label: 'Contact Us', key: '4' },
-  ];
+  { href: 'about-us', label: 'About Us', key: '1' },
+  { href: 'production-processes', label: 'Production processes', key: '2' },
+  { href: 'post-processing', label: 'Post processing', key: '3'},
+  { href: 'sample-parts', label: 'Some sample parts', key: '4' },
+  { href: 'contact-us', label: 'Contact Us', key: '5' },
+  { href: 'request', label: 'Send us a request', key: '6'}
+];
