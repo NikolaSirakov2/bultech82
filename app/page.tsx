adb1129caf9d9aa3fcdc8aec42f5b48711d97045
NikolaSirakov2 <nikolasirakov1@gmail.com>
2024-04-02 15:08:16 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import AboutUs from "@/app/components/AboutUs";
 import ContactUs from "@/app/components/ContactUs";
 import Hero from "@/app/components/Hero";
 import Services from "@/app/components/Services";
+import Processing from "@/app/components/Processing";
+import SampleParts from "@/app/components/SampleParts";
 import React from "react";
 
 export default function Home() {
@@ -9,7 +11,9 @@ export default function Home() {
     <>
       <div id="hero"><Hero /></div>
       <div id="about-us"><AboutUs /></div>
-      <div id="services"><Services /></div>
+      <div id="production-processes"><Services /></div>
+      <div id="post-processing"><Processing /></div>
+      <div id="sample-parts"><SampleParts /></div>
       <div id="contact-us"><ContactUs /></div>
     </>
   );
