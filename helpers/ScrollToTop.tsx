e33015ac764f056d6ec40043e4dd9ce23e4c6703
NikolaSirakov2 <nikolasirakov1@gmail.com>
2024-04-05 05:00:51 +0300

    Update ScrollToTop.tsx

--- helpers/ScrollToTop.tsx
+++ helpers/ScrollToTop.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import { useEffect, useState } from "react";
+import Image from "next/image";
 
 export default function ScrollToTop() {
   const [isVisible, setIsVisible] = useState(false);
 
   const toggleVisibility = () => {
-    if (window.pageYOffset > 300) {
+    if (window.scrollY > 300) {
       setIsVisible(true);
     } else {
       setIsVisible(false);
@@ -33,9 +34,8 @@ export default function ScrollToTop() {
           position: "fixed",
           bottom: "20px",
           right: "20px",
-          backgroundColor: "gray",
-          height: "40px",
-          width: "40px",
+          height: "72px",
+          width: "72px",
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
@@ -43,20 +43,12 @@ export default function ScrollToTop() {
           cursor: "pointer",
         }}
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="white"
-          className="h-6 w-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth={2}
-            d="M5 10l7-7m0 0l7 7m-7-7v18"
-          />
-        </svg>
+        <Image
+          src="/scrolltotop.svg"
+          alt="Scroll to top"
+          width={96}
+          height={96}
+        />
       </div>
     )
   );
