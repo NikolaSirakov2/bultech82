e032fb44cac601e34813e10b30ccee3ac7fda45e
NikolaSirakov2 <nikolasirakov1@gmail.com>
2024-04-05 10:08:05 +0300

    Create request.tsx

---
+++ pages/request.tsx
@@ -0,0 +1,14 @@
+import "../app/globals.css";
+
+const RequestPage = () => {
+    return (
+        <div>
+            <h1 className="text-4xl font-bold text-gray-800 md:text-5xl xl:text-5xl">
+                Send us a request
+            </h1>
+            {/* Add your form or other content here */}
+        </div>
+    );
+};
+
+export default RequestPage;
