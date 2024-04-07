a1464df17745235279f2edf3a184debd4e744807
NikolaSirakov2 <nikolasirakov1@gmail.com>
2024-04-07 10:04:31 +0300

    Update Services.tsx

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -6,13 +6,12 @@ function Services() {
       id="services"
       className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20"
     >
-       <h1 className="text-4xl font-bold text-gray-800 md:text-5xl xl:text-5xl">
-            Production Processes
-          </h1>
+      <h1 className="text-4xl font-bold text-gray-800 md:text-5xl xl:text-5xl">
+        Production Processes
+      </h1>
       <div className="flex flex-col md:flex-row gap-10 md:gap-12 xl:gap-16">
-        <div className="flex flex-row gap-3 md:w-4/4">
-
-          <div className="flex flex-col items-center space-y-4 md:flex-col">
+        <div className="flex flex-col md:flex-row gap-3 md:w-full">
+          <div className="flex flex-col items-center space-y-4 md:flex-col md:items-start">
             <div className="relative h-106 w-full md:w-3/4">
               <img
                 src="/Image2.jpg"
@@ -26,7 +25,7 @@ function Services() {
             </p>
           </div>
 
-          <div className="flex flex-col items-center space-y-4 md:flex-col">
+          <div className="flex flex-col-reverse md:flex-col items-center space-y-4 md:items-start">
             <p className="text-1xl text-gray-600 md:text-1xl xl:text-2xl">
               Laser cutting and bending sheet, Marking laser, CNC and manual
               milling, CNC and manual turning, EDM
