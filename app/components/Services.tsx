42898e44982e8a56052a9e25d31c5ec570055c14
NikolaSirakov2 <nikolasirakov1@gmail.com>
2024-04-08 09:09:51 +0300

    Update Services.tsx

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -10,6 +10,21 @@ function Services() {
         Production Processes
       </h1>
       <div className="flex flex-col md:flex-row gap-10 md:gap-12 xl:gap-16">
+        <div className="flex flex-col-reverse md:flex-col items-center space-y-4 md:items-start">
+          <p className="text-1xl text-gray-600 md:text-1xl xl:text-2xl">
+            Laser cutting and bending sheet, Marking laser, CNC and manual
+            milling, CNC and manual turning, EDM
+          </p>
+
+          <div className="relative h-106 w-full md:w-3/4">
+            <img
+              src="/Image3.jpg"
+              alt="Machine Parts"
+              className="object-cover object-center h-full w-full"
+            />
+          </div>
+        </div>
+
         <div className="flex flex-col md:flex-row gap-3 md:w-full">
           <div className="flex flex-col items-center space-y-4 md:flex-col md:items-start">
             <div className="relative h-106 w-full md:w-3/4">
@@ -23,21 +38,6 @@ function Services() {
             <p className="text-1xl text-gray-600 md:text-1xl xl:text-2xl">
               Grinding, Tapping, Punching, Welding, Manual assembly
             </p>
-          </div>
-
-          <div className="flex flex-col-reverse md:flex-col items-center space-y-4 md:items-start">
-            <p className="text-1xl text-gray-600 md:text-1xl xl:text-2xl">
-              Laser cutting and bending sheet, Marking laser, CNC and manual
-              milling, CNC and manual turning, EDM
-            </p>
-
-            <div className="relative h-106 w-full md:w-3/4">
-              <img
-                src="/Image3.jpg"
-                alt="Machine Parts"
-                className="object-cover object-center h-full w-full"
-              />
-            </div>
           </div>
         </div>
       </div>
