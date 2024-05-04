371ccf745bd2a39dd08b114de237557c879df9be
NikolaSirakov2 <nikolasirakov1@gmail.com>
2024-05-04 13:02:50 +0300

    Update request.tsx

--- pages/request.tsx
+++ pages/request.tsx
@@ -11,16 +11,10 @@ const RequestPage = () => {
   const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0];
     if (file && file.type === "application/pdf") {
-      // replace "application/pdf" with the file type you want to accept
       setFile(file);
     } else {
       alert("Invalid file format");
     }
-  };
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    // handle form submission here
   };
 
   return (
@@ -42,14 +36,14 @@ const RequestPage = () => {
         />
       </Link>
       <div className="bg-white border border-black rounded-lg p-4 w-full max-w-3xl mt-8">
-      <form onSubmit={handleSubmit} className="w-full max-w-3xl mt-8 bg-white">
+      <form method="POST" action="https://formsubmit.co/nikolascoaching@gmail.com" className="w-full max-w-3xl mt-8 bg-white">
         <div className="flex flex-col sm:flex-row space-y-4 sm:space-y-0 sm:space-x-4">
           <div className="flex flex-col w-full">
             <label htmlFor="email" className="mb-2">
               Email
             </label>
             <input
-              id="email"
+              name="email"
               type="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
@@ -63,7 +57,7 @@ const RequestPage = () => {
               File
             </label>
             <input
-              id="file"
+              name="file"
               type="file"
               onChange={handleFileChange}
               className="border-2 border-gray-600 rounded-lg p-4 mb-4 w-full text-xl"
@@ -75,7 +69,7 @@ const RequestPage = () => {
             Message
           </label>
           <textarea
-            id="message"
+            name="message"
             value={message}
             onChange={(e) => setMessage(e.target.value)}
             required
