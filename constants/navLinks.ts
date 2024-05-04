74fcf270952f707e93c1eca0dd6b0c1326199c4f
NikolaSirakov2 <nikolasirakov1@gmail.com>
2024-05-04 13:02:47 +0300

    Update navLinks.ts

--- constants/navLinks.ts
+++ constants/navLinks.ts
@@ -6,3 +6,12 @@ export const NAV_LINKS = [
   { href: 'contact-us', label: 'Contact Us', key: '5' },
   { href: 'request', label: 'Send us a request', key: '6'}
 ];
+
+export const NAV_LINKS_BG = [
+  { href: 'about-us', label: 'За нас', key: '1' },
+  { href: 'production-processes', label: 'Процеси на изработка', key: '2' },
+  { href: 'post-processing', label: 'Довършителни процеси', key: '3'},
+  { href: 'sample-parts', label: 'Примерни детайла', key: '4' },
+  { href: 'contact-us', label: 'Контакти', key: '5' },
+  { href: 'request', label: 'Изпрати запитване', key: '6'}
+];
