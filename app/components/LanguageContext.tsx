fc5701e0d87e98ae87340bbd8b9757241f1c1af3
NikolaSirakov2 <nikolasirakov1@gmail.com>
2024-05-06 15:49:55 +0300

    refactor: Improve language handling in LanguageContext.tsx

--- app/components/LanguageContext.tsx
+++ app/components/LanguageContext.tsx
@@ -9,24 +9,26 @@ interface LanguageContextValue {
 const LanguageContext = createContext<LanguageContextValue | undefined>(undefined);
 
 export const LanguageProvider = ({ children }: { children: React.ReactNode }) => {
-  const [language, setLanguage] = useState('BG'); // default language
+ const [language, setLanguage] = useState('BG'); // default language
 
-  useEffect(() => {
-    const storedLanguage = typeof window !== 'undefined' ? localStorage.getItem('language') : 'BG';
-    if (storedLanguage) {
-      setLanguage(storedLanguage);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      localStorage.setItem('language', language);
-    }
-  }, [language]);
+useEffect(() => {
+  const storedLanguage = typeof window !== 'undefined' ? localStorage.getItem('language') : 'BG';
+  if (storedLanguage) {
+    setLanguage(storedLanguage);
+  }
+}, []);
 
   return (
     <LanguageContext.Provider value={{ language, setLanguage }}>
       {children}
     </LanguageContext.Provider>
   );
+};
+
+export const useLanguage = (): LanguageContextValue => {
+  const context = useContext(LanguageContext);
+  if (!context) {
+    throw new Error('useLanguage must be used within a LanguageProvider');
+  }
+  return context;
 };
