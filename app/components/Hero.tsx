d634edf6d3b3f531716729645e7a129d317df317
NikolaSirakov2 <nikolasirakov1@gmail.com>
2025-02-28 20:17:23 +0800

    chore: Update dependencies and refactor Hero and SampleParts components

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -9,21 +9,27 @@ function Hero() {
   return (
     <section className="max-container padding-container flex flex-col gap-20 py-10 pb-8 md:gap-28 lg:py-20">
       <div className="flex flex-col gap-10 md:gap-12 xl:gap-16">
-        
         <div className="flex flex-col gap-3">
           <h1 className="text-4xl font-bold text-gray-800 md:text-5xl xl:text-6xl">
-            {language === 'BG' ? 'Bultech82 Ltd' : 'Bultech82 ООД'}
+            {language === "BG" ? "Bultech82 Ltd" : "Bultech82 ООД"}
           </h1>
 
           <h3
             className={`${style.h3SmallScreen} text-2xl text-gray-600 md:text-3xl xl:text-4xl`}
           >
-            {language === 'BG' ? 'Bulgarian company for manufacturing technologies' : 'Българска компания за производствени технологии'}
+            {language === "BG"
+              ? "Bulgarian company for manufacturing technologies"
+              : "Българска компания за производствени технологии"}
           </h3>
         </div>
 
         <div className={style.imageContainer}>
-          <Image src="/снимка 1.2.png" alt="Machine Parts" layout="fill" objectFit="cover" />
+          <Image
+            src="/снимка 1.2.png"
+            alt="Machine Parts"
+            fill
+            className="object-cover"
+          />
         </div>
       </div>
     </section>
--- app/components/SampleParts.tsx
+++ app/components/SampleParts.tsx
@@ -14,37 +14,43 @@ const samples = [
   {
     name: "1",
     text: "Material: Steel S235JR\nProduction process: Turning, Milling\nPost process: Zincing (not shown on the picture)",
-    textBG: "Материал: Стомана S235JR\nПроцес на производство: Токарене, Фрезоване\nПоследваща обработка: Поцинковане (не е показано на снимката)",
+    textBG:
+      "Материал: Стомана S235JR\nПроцес на производство: Токарене, Фрезоване\nПоследваща обработка: Поцинковане (не е показано на снимката)",
     image: "/newElement6.jpg",
   },
   {
     name: "2",
     text: "Material: Stainless Steel 304\nProduction process: Laser cutting, CNC Bending, Manual deburring and cleaning\nPost process: Electropolishing",
-    textBG: "Материал: Неръждаема стомана 304\nПроцес на производство: Лазерно рязане, CNC Буксиране, Ръчно обелване и почистване\nПоследваща обработка: Електрополиране",
+    textBG:
+      "Материал: Неръждаема стомана 304\nПроцес на производство: Лазерно рязане, CNC Буксиране, Ръчно обелване и почистване\nПоследваща обработка: Електрополиране",
     image: "/newElement1.jpg",
   },
   {
     name: "3",
     text: "Material: Aluminium EN AW-5754\nProduction process: Laser cutting, Deburring, Milling\nPost process: Anodising (black)",
-    textBG: "Материал: Алуминий EN AW-5754\nПроцес на производство: Лазерно рязане, Обелване, Фрезоване\nПоследваща обработка: Анодиране (черно)",
+    textBG:
+      "Материал: Алуминий EN AW-5754\nПроцес на производство: Лазерно рязане, Обелване, Фрезоване\nПоследваща обработка: Анодиране (черно)",
     image: "/newElement2.jpg",
   },
   {
     name: "4",
     text: "Material: Steel S235JR\nProduction process: Laser cutting, CNC Bending,\nManual deburring and cleaning\nPost process: Powder coating RAL 9005, high gloss",
-    textBG: "Материал: Стомана S235JR\nПроцес на производство: Лазерно рязане, CNC Буксиране,\nРъчно обелване и почистване\nПоследваща обработка: Прахово боядисване RAL 9005, висок блясък",
+    textBG:
+      "Материал: Стомана S235JR\nПроцес на производство: Лазерно рязане, CNC Буксиране,\nРъчно обелване и почистване\nПоследваща обработка: Прахово боядисване RAL 9005, висок блясък",
     image: "/newElement3.jpg",
   },
   {
     name: "5",
     text: "Material: Steel S235JR\nProduction process: Laser cutting, CNC Milling, Tumbling\nPost process: Zincing",
-    textBG: "Материал: Стомана S235JR\nПроцес на производство: Лазерно рязане, CNC Фрезоване, Полиране\nПоследваща обработка: Поцинковане",
+    textBG:
+      "Материал: Стомана S235JR\nПроцес на производство: Лазерно рязане, CNC Фрезоване, Полиране\nПоследваща обработка: Поцинковане",
     image: "/newElement4.jpg",
   },
   {
     name: "6",
     text: "Material: Aluminium EN AW-6082\nProduction process: Laser cutting, Manual deburring and cleaning, Tapping\nPost process: Anodising (natural)",
-    textBG: "Материал: Алуминий EN AW-6082\nПроцес на производство: Лазерно рязане, Ръчно обелване и почистване, Резбоване\nПоследваща обработка: Анодиране (естествен)",
+    textBG:
+      "Материал: Алуминий EN AW-6082\nПроцес на производство: Лазерно рязане, Ръчно обелване и почистване, Резбоване\nПоследваща обработка: Анодиране (естествен)",
     image: "/newElement5.jpg",
   },
 ];
@@ -106,11 +112,11 @@ function SampleParts() {
               className="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/3 transform transition-transform duration-200 outline-transparent outline-2 mr-2 hover:scale-105 hover:cursor-pointer" // Adjusted card size
               onMouseDown={() => setSelectedSample(sample)}
             >
-              <div className="w-full h-96 relative">
+              <div className="relative aspect-square w-full">
                 <Image
                   src={sample.image}
                   alt={sample.name}
-                  layout="fill"
+                  fill
                   className="absolute object-cover object-center"
                 />
                 {index !== samples.length - 1 && (
